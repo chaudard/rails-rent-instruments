ae8bb450228256a6c9843b4659f9fb8c4987605a
chaudard <donnetonfalzard+github@gmail.com>
2018-05-29 10:48:50 +0200

    add booking model, validations, update user model

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,2 +1,13 @@
 class Instrument < ApplicationRecord
+  has_many :bookings
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :price, presence: true, numericality: true
+  validates :image, presence: true
+  validates :address, presence: true
+  validates :zipcode, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_28_164548) do
+ActiveRecord::Schema.define(version: 2018_05_29_084013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "rating"
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "validated"
+    t.bigint "user_id"
+    t.bigint "instrument_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_id"], name: "index_bookings_on_instrument_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "instruments", force: :cascade do |t|
     t.string "title"
@@ -46,9 +59,15 @@ ActiveRecord::Schema.define(version: 2018_05_28_164548) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "avatar"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "instruments"
+  add_foreign_key "bookings", "users"
   add_foreign_key "instruments", "users"
 end
