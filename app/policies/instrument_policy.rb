1831c0937c2c61d794141ce4d5609b18c6c365b8
jeoffrey depinoy <jdepinoy@norauto.com>
2018-05-29 17:32:13 +0200

    homepage

--- app/policies/instrument_policy.rb
+++ app/policies/instrument_policy.rb
@@ -1,7 +1,26 @@
 class InstrumentPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
   end
+
+  def new?
+    true
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    record.user == user
+  # - record: the user passed to the `authorize` method in controller
+  # - user:   the `current_user` signed in with Devise.
+  end
+
 end
