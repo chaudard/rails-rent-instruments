0fcbf19b105c6bc27e2db2bba9daea70d8178881
jeoffrey depinoy <jdepinoy@norauto.com>
2018-05-30 18:33:08 +0200

    booking creation via instrument show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,32 @@
 class BookingsController < ApplicationController
+  skip_after_action :verify_authorized
+
   def index
+
   end
 
   def show
+    authorize @booking
   end
 
   def new
+
   end
 
   def create
+    @booking = Booking.new(params_booking)
+    instrument = Instrument.find(params[:instrument_id])
+    @booking.instrument = instrument
+    @booking.user = current_user
+    @booking.start_date = params["booking"]["start_date"].to_date
+    @booking.end_date = params["booking"]["end_date"].to_date
+
+    if @booking.save
+      redirect_to instrument_path(instrument), notice: 'Booking validated'
+    else
+
+    end
+
   end
 
   private
